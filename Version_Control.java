7ed9d9b9b0c6060c973c41ad8105a60899ba2a65
murtazask <31232598+murtazask@users.noreply.github.com>
2017-08-24 12:02:12 +0530

    First_Version

---
+++ Version_Control.java
@@ -0,0 +1,36 @@
+package version_control;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+
+public class Version_Control {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Login");
+		frame.setLayout(null);
+		JTextField user = new JTextField(10);
+		JTextField pass = new JTextField(10);
+		JLabel l = new JLabel("Username:");
+		JLabel l2 = new JLabel("Password:");
+		JButton button = new JButton("Login");
+		user.setBounds(140, 100, 100, 20);
+		pass.setBounds(140, 140, 100, 20);
+		button.setBounds(140, 180, 100, 20);
+		l.setBounds(70, 100, 100, 20);
+		l2.setBounds(70, 140, 100, 20);
+		frame.add(user);
+		frame.add(pass);
+		frame.add(l);
+		frame.add(l2);
+		frame.add(button);
+		
+		frame.setSize(400,400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocation(500, 150);
+		frame.setVisible(true);
+	}
+
+}
